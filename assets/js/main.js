8ab00b42b8e844ec6183fceaa37ef79e71e158a6
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-22 14:18:35 +0200

    add calcolaEta method

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,10 +28,18 @@ class Veicolo {
     informazioni() {
         return `E' una ${this.marca} del ${this.anno}, di colore ${this.colore}, con ${this.porte} porte che va a ${this.carburante}.`;
     };
+
+    // metodo per calcolare l'anno della vettura
+    calcolaEta() {
+        // ottengo l anno attuale
+        const annoAttuale = new Date().getFullYear();
+        // faccio la differenza
+        return `La macchina ha ${annoAttuale - this.anno} anni`;
+    }
 };
 
 // creo un'instanza 
 const veicoloOne = new Veicolo("Fiat", 2019, "blu", 4, "benzina");
-
 console.log(veicoloOne.informazioni());
+console.log(veicoloOne.calcolaEta());
 
