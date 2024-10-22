61932a7a291ab0b274119b1c1e323a27f1f7b345
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-10-22 14:51:33 +0200

    add second class from the main classe

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,18 @@
-/*
-
-Successivamente:
-Creiamo una nuova istanza di veicolo che rappresenti una Fiat 2019 blu.
-Otteniamo le informazioni della vettura utilizzando il metodo informazioni()
-Calcoliamo l'età della vettura utilizzando il metodo calcolaEta()
-
-
-*/
-
 // creo una classe che rappresenta un veicolo
 class Veicolo {
     marca;
     anno;
     colore;
-    porte;
-    carburante;
 
-    constructor(marca, anno, colore, porte, carburante) {
+    constructor(marca, anno, colore) {
         this.marca = marca;
         this.anno = anno;
         this.colore = colore;
-        this.porte = porte;
-        this.carburante = carburante;
     };
 
     // metodo per ottenere le info della vettura
     informazioni() {
-        return `E' una ${this.marca} del ${this.anno}, di colore ${this.colore}, con ${this.porte} porte che va a ${this.carburante}.`;
+        return `E' una ${this.marca} del ${this.anno}, di colore ${this.colore}.`;
     };
 
     // metodo per calcolare l'anno della vettura
@@ -35,11 +21,33 @@ class Veicolo {
         const annoAttuale = new Date().getFullYear();
         // faccio la differenza
         return `La macchina ha ${annoAttuale - this.anno} anni`;
-    }
+    };
 };
 
-// creo un'instanza 
-const veicoloOne = new Veicolo("Fiat", 2019, "blu", 4, "benzina");
+// creo una classe figlia di Veicolo
+class Automobile extends Veicolo {
+    porte;
+    carburante;
+
+    constructor(marca, anno, colore, porte, carburante) {
+        // super serve per richiamare il costruttore della classe genitore
+        super(marca, anno, colore);
+        this.porte = porte;
+        this.carburante = carburante;
+    }
+
+    // metodo per ottenere le informazioni 
+    informazioniAuto() {
+        // posso richiamare il metodo del genitore
+        return `${this.informazioni()} Ha ${this.porte} porte e va a ${this.carburante}`;
+    }
+}
+
+// creo due instanze ovvero due oggetti dalla classe
+const veicoloOne = new Veicolo("Fiat", 2019, "blu");
 console.log(veicoloOne.informazioni());
 console.log(veicoloOne.calcolaEta());
+
+const veicoloTwo = new Automobile("Opel", 2020, "nera", 4, "benzina");
+console.log(veicoloTwo.informazioniAuto());
 
